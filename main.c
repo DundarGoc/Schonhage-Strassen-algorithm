de745f3ff61c1b6344dc49ed0fd1d60cd554c435
Dundar Göc <gocdundar@gmail.com>
2019-08-13 18:15:48 +0200

    main.c: Added test example.

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
+  int *a = malloc(sizeof(int) * 8);
+  int *b = malloc(sizeof(int) * 8);
+  int *c = malloc(sizeof(int) * 8);
+
   for(int i=0; i<8; ++i){
-    printf("%i\n", i); 
+    a[i]=8-i;
+    b[i]=i;
   }
 
+  for(int i=0; i<8; ++i){
+    c[i]=a[i]-b[i];
+  }
 
+  free(a);
+  free(b);
+  free(c);
 
 }
