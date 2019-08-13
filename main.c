ecf78bc8498a6e5fb123420792fed014d55356a7
Dundar Göc <gocdundar@gmail.com>
2019-08-13 18:40:15 +0200

    main.c: Random input.

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(){
+
+  srand(time(NULL));
 
   int *a = malloc(sizeof(int) * 8);
   int *b = malloc(sizeof(int) * 8);
   int *c = malloc(sizeof(int) * 8);
 
   for(int i=0; i<8; ++i){
-    a[i]=8-i;
-    b[i]=i;
+    a[i]=rand()%10;
+    b[i]=rand()%10;
   }
 
   for(int i=0; i<8; ++i){
